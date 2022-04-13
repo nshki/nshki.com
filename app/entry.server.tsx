8edf71a22a90edeb65c8fc9e1372b7db73b53709
Nishiki Liu <hello@nshki.com>
2022-04-12 17:41:30 -0700

    Set ETags based on document markup

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,7 @@
-import { RemixServer } from '@remix-run/react'
 import type { EntryContext } from '@remix-run/node'
+import { RemixServer } from '@remix-run/react'
 import { renderToString } from 'react-dom/server'
+import etag from 'etag'
 
 export default function handleRequest (
   request: Request,
@@ -8,13 +9,18 @@ export default function handleRequest (
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(
+  let markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   )
 
-  responseHeaders.set("Content-Type", "text/html")
+  responseHeaders.set('Content-Type', 'text/html')
+  responseHeaders.set('Etag', etag(markup))
 
-  return new Response("<!DOCTYPE html>" + markup, {
+  if (request.headers.get('If-None-Match') === responseHeaders.get('Etag')) {
+    return new Response('', { status: 304, headers: responseHeaders })
+  }
+
+  return new Response('<!DOCTYPE html>' + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
   })
