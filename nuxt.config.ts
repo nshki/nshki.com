118a14d41407454f44ec2fdbe12dfcc9fa7cd9a2
Nishiki Liu <hello@nshki.com>
2025-09-29 21:45:00 -0700

    Enable experimental @nuxt/content config
    This is to avoid having to install an unnecessary SQLite package into
    the project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxt/test-utils"],
   content: {
+    experimental: { sqliteConnector: "native" },
     build: {
       markdown: {
         highlight: {
