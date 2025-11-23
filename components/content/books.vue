e08da69c6e1358ff78b7c1e75f68dd2422e90008
Nishiki Liu <hello@nshki.com>
2025-11-23 11:12:49 -0800

    Add `<Books>` content

---
+++ components/content/books.vue
@@ -0,0 +1,81 @@
+<template>
+  <Grid more-spacing>
+    <Book
+      current
+      coverAssetUrl="/assets/pages/today/careless-people.jpg"
+      title="Careless People"
+      author="Sarah Wynn-Williams"
+    >
+      <p>
+        Working through this currently, but the ex-director of public policy at Meta
+        details her experience working there. She writes in a way that makes me feel
+        like I can really see through her eyes.
+      </p>
+
+      <p>Immersive, captivating, and horrifying.</p>
+    </Book>
+
+    <Book
+      coverAssetUrl="/assets/pages/today/the-jakarta-method.jpg"
+      title="The Jakarta Method"
+      author="Vincent Bevins"
+    >
+      <p>
+        This book made me realize that I actually like history, despite my academic
+        experience with it. This details, with research through various primary sources,
+        the <em>bogglingly</em> bloody history of the United States' involvement with
+        foreign regimes in the 20th century.
+      </p>
+    </Book>
+
+    <Book
+      coverAssetUrl="/assets/pages/today/us.jpg"
+      title="Us"
+      author="Terrence Real"
+    >
+      <p>
+        Terry Real lays the framework for relational life therapy, where he details the
+        specific skills needed to reframe "you vs me" as "us vs the issue" in
+        relationships. Incredible insight through real-life sessions with his patients
+        and his own life.
+      </p>
+    </Book>
+
+    <Book
+      coverAssetUrl="/assets/pages/today/the-courage-to-be-disliked.jpg"
+      title="The Courage To Be Disliked"
+      author="Ichiro Kishimi & Fumitake Koga"
+    >
+      <p>
+        An intensly thought-provoking dialogue between a youth and an philosopher about
+        framing life in a different way to find true happiness. Although the dialogue is
+        in the context of modern life, there are still Japanese cultural biases that sneak
+        in, which I found to be fun because I'm a third-culture kid in the US.
+      </p>
+    </Book>
+
+    <Book
+      coverAssetUrl="/assets/pages/today/what-my-bones-know.jpg"
+      title="What My Bones Know"
+      author="Stephanie Foo"
+    >
+      <p>
+        An unapologetically raw re-telling of traumatic events in Stephanie's life that led
+        to the development of her complex PTSD. Her insights capture heavy, powerful ideas
+        that helped shape how I view mental health and just being human in general.
+      </p>
+    </Book>
+
+    <Book
+      coverAssetUrl="/assets/pages/today/know-my-name.jpg"
+      title="Know My Name"
+      author="Chanel Miller"
+    >
+      <p>
+        A memoir about sexual assault, systematic and cultural failures to support women,
+        and the healing journey. Chanel Miller's courage and ability to write this book was
+        incredibly moving despite all odds against her.
+      </p>
+    </Book>
+  </Grid>
+</template>
