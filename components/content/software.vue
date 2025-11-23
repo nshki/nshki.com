4c03a62b1a31790ebf353d3817b6f4dd9e530fe9
Nishiki Liu <hello@nshki.com>
2025-11-23 11:13:01 -0800

    Add various other content components

---
+++ components/content/software.vue
@@ -0,0 +1,39 @@
+<template>
+  <dl>
+    <dt>Browser</dt>
+    <dd>Zen Browser</dd>
+
+    <dt>Editor</dt>
+    <dd>LazyVim</dd>
+
+    <dt>Terminal</dt>
+    <dd>Ghostty</dd>
+
+    <dt>Shell</dt>
+    <dd>Bash</dd>
+
+    <dt>Containers</dt>
+    <dd>OrbStack</dd>
+
+    <dt>Password Manager</dt>
+    <dd>1Password</dd>
+
+    <dt>Mail Provider</dt>
+    <dd>Fastmail</dd>
+
+    <dt>Fedi Client</dt>
+    <dd>Phanpy</dd>
+
+    <dt>Smart Home</dt>
+    <dd>Home Assistant</dd>
+
+    <dt>Photos</dt>
+    <dd>Ente (Self-Hosted)</dd>
+
+    <dt>Books</dt>
+    <dd>Libby</dd>
+
+    <dt>Mobile Browser</dt>
+    <dd>DuckDuckGo</dd>
+  </dl>
+</template>
