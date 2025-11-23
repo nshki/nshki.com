acb5d7286576f4dd5ced200c75be6e8d8b3ab622
Nishiki Liu <hello@nshki.com>
2025-11-23 11:12:29 -0800

    Add `<Badge>` and `<Book>` components

---
+++ components/book.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+defineProps({
+  current: Boolean,
+  coverAssetUrl: String,
+  title: String,
+  author: String
+})
+</script>
+
+<template>
+  <article class="book">
+    <img
+      class="book__cover"
+      :src="coverAssetUrl"
+      :alt="`${title} by ${author}`"
+    />
+
+    <div class="book__content">
+      <h3 class="book__title">
+        {{ title }}
+
+        <Badge v-if="current">Current</Badge>
+        <Badge inactive v-else>Read</Badge>
+      </h3>
+
+      <p class="book__author">{{ author }}</p>
+
+      <div class="book__description">
+        <slot />
+      </div>
+    </div>
+  </article>
+</template>
+
+<style>
+.book {
+  display: grid;
+  grid-template-columns: 0.3fr 0.7fr;
+  gap: 24px;
+  position: relative;
+}
+
+.book__cover {
+  width: 100%;
+  height: auto;
+}
+
+.book__title {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.book__author {
+  margin-top: 0;
+  font-size: 1rem;
+  font-style: italic;
+}
+
+.book__description {
+  & > * {
+    font-size: 1rem;
+  }
+}
+</style>
